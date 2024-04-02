690bf43216d6c00eef5b2d5673769a98742e1c1f
Peter <mendez.pedro94@gmail.com>
2024-04-01 22:08:00 -0300

    add navbar change color

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,13 @@ $(".hide-3").click(function(event) { // selecciona la clase hide-3
 $(".hide-4").click(function(event) { // selecciona la clase hide-3
     $(".text-4").toggle('slow'); // oculta o muestra la clase text-3
 });
+
+// color negro al bajar el scroll
+$(document).scroll( function(e){
+    const y = $("html").scrollTop();
+    if (y > 500) {
+        $("nav").addClass("nav-black");
+    } else {
+        $("nav").removeClass("nav-black");
+    }
+});
